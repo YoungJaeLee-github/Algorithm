40e8c585004e5627a2c9d48045dbda7d9598f67c
iyeongjae <dudwo8075@gmail.com>
2021-01-24 20:52:44 +0900

    Algorithm Ex

---
+++ src/StackEx2.java
@@ -0,0 +1,30 @@
+// bracket Ex
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class StackEx2 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        int testCase = Integer.parseInt(bufferedReader.readLine());
+
+        while (testCase != 0) {
+            String input = bufferedReader.readLine();
+            int openBracketCount = 0;
+
+            for (int i = 0; i < input.length(); i++) {
+                if (input.charAt(i) == '(')
+                    openBracketCount++;
+                else
+                    openBracketCount--;
+                if (openBracketCount < 0) {
+                    break;
+                }
+            }
+            System.out.println(openBracketCount == 0 ? "YES" : "NO");
+            testCase--;
+        }
+        bufferedReader.close();
+    }
+}
