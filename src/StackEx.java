c2ad17114ab4f167e72746a620857c63296d48a4
iyeongjae <dudwo8528@gmail.com>
2021-01-24 19:55:02 +0900

    ReverseWord Using Stack

--- src/StackEx.java
+++ src/StackEx.java
@@ -1,7 +1,9 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 
-public class ReverseWordByStack {
+public class StackEx {
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
         int testCase = Integer.parseInt(bufferedReader.readLine());
