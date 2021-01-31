181522b8782255d2a0d4c0523829e5aa7b773149
YounJaeLee <dudwo8075@gmail.com>
2021-02-01 00:01:13 +0900

    Algorithm Ex

---
+++ src/StackEx3.java
@@ -0,0 +1,40 @@
+import java.io.*;
+import java.util.Stack;
+
+public class StackEx3 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        Stack<Integer> stack = new Stack<>();
+
+        int n = Integer.parseInt(bufferedReader.readLine());
+        int temp = 0;
+        StringBuilder result = new StringBuilder();
+
+        while (n != 0) {
+            int number = Integer.parseInt(bufferedReader.readLine());
+            if (temp < number) {
+                while (temp < number) {
+                    stack.push(++temp);
+                    result.append("+\n");
+                }
+                stack.pop();
+                result.append("-\n");
+            } else {
+                boolean found = false;
+                if (!stack.isEmpty()) {
+                    result.append("-\n");
+                    if (stack.pop() == number) {
+                        found = true;
+                    }
+                }
+                if (!found) {
+                    System.out.print("NO");
+                    return;
+                }
+            }
+            n--;
+        }
+        System.out.print(result);
+        bufferedReader.close();
+    }
+}
---
+++ src/StackEx4.java
@@ -0,0 +1,57 @@
+// 커서를 기준으로 문자가 삽입/삭제/이동 되므로 2개의 Stack 또는 LinkedList로 구현할 수 있음.
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
+public class StackEx4 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        Stack<Character> leftStack = new Stack<>();
+        Stack<Character> rightStack = new Stack<>();
+
+        String input = bufferedReader.readLine();
+        for (int i = 0; i < input.length(); i++) {
+            leftStack.push(input.charAt(i));
+        }
+
+        int M = Integer.parseInt(bufferedReader.readLine());
+        while (M-- > 0) {
+            String command = bufferedReader.readLine();
+            switch (command) {
+                case "L":
+                    if (leftStack.isEmpty())
+                        break;
+                    else
+                        rightStack.push(leftStack.pop());
+                    break;
+                case "D":
+                    if (rightStack.isEmpty())
+                        break;
+                    else
+                        leftStack.push(rightStack.pop());
+                    break;
+                case "B":
+                    if (leftStack.isEmpty())
+                        break;
+                    else
+                        leftStack.pop();
+                    break;
+                default:
+                    String[] parse = command.split(" ");
+                    leftStack.push(parse[1].charAt(0));
+            }
+        }
+        while (!leftStack.isEmpty()) {
+            rightStack.push(leftStack.pop());
+        }
+        StringBuilder stringBuilder = new StringBuilder();
+
+        while (!rightStack.isEmpty()) {
+            stringBuilder.append(rightStack.pop());
+        }
+        System.out.println(stringBuilder);
+        bufferedReader.close();
+    }
+}
